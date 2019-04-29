376f299797fa8a6e8267d1ec7595f5408d8965bc
priya276 <47981705+priya276@users.noreply.github.com>
2019-04-29 14:46:59 +0530

    Update HelloTest.java

--- src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.*;
 public class HelloTest {
 
     @Test
-    public void testSayHello() {
+     void testSayHello() {
         OutputStream os = new ByteArrayOutputStream();
         PrintStream stream = new PrintStream(os, true);
 
